a4e7a7449c0de92d62b81a713c12c263a302555c
Kovi <139319195+RogueChocoholic@users.noreply.github.com>
2024-08-04 00:37:10 +0530

    errors
    uploading error happend.
    uploaded everything again

--- script.js
+++
@@ -1,8 +0,0 @@
-function downloadPdf(){
-    var link = document.createElement('a');
-    link.href = "../resources/cv/Chanuths' CV (1) (1).pdf";
-    link.download;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
