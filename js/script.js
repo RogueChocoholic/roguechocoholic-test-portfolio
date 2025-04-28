492dbdfe74af1f34f945225ded376363a33d00de
Kovi <139319195+RogueChocoholic@users.noreply.github.com>
2025-04-28 22:50:56 +0530

    new project
    added pjbiology to projects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-function downloadPdf(){
-    var link = document.createElement('a');
-    link.href = "../resources/cv/Kovidha Subasinghe Resume.pdf";
-    link.download;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+function downloadPdf() {
+  var link = document.createElement("a");
+  link.href = "../resources/cv/Kovidha Subasinghe Resume.pdf";
+  link.download;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
+
+// Smoothly fade-in sections
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".fade-in").forEach((el) => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 50) {
+      el.classList.add("active");
+    }
+  });
+});
